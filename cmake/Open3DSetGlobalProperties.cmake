774390780b7535936be77717ee283fe93817005f
Yixing Lao <yixing.lao@gmail.com>
2021-09-23 03:07:29 -0700

    turn off fast math

--- cmake/Open3DSetGlobalProperties.cmake
+++ cmake/Open3DSetGlobalProperties.cmake
@@ -109,6 +109,11 @@ function(open3d_set_global_properties target)
         target_compile_options(${target} PRIVATE $<$<COMPILE_LANGUAGE:ISPC>:--arch=x86-64>)
     endif()
 
+    # Turn off fast math for IntelLLVM DPC++ compiler
+    # TODO: make this optional and tune unit tests floating point precisions.
+    target_compile_options(${target} PRIVATE
+        $<$<AND:$<CXX_COMPILER_ID:IntelLLVM>,$<NOT:$<COMPILE_LANGUAGE:ISPC>>>:-fno-fast-math>)
+
     # TBB static version is used
     # See: https://github.com/wjakob/tbb/commit/615d690c165d68088c32b6756c430261b309b79c
     target_compile_definitions(${target} PRIVATE __TBB_LIB_NAME=tbb_static)
