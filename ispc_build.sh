228445529ac0a9c90e7f0e038dd059880136a15c
Yixing Lao <yixing.lao@gmail.com>
2021-09-23 02:41:52 -0700

    add unit test log

--- ispc_build.sh
+++ ispc_build.sh
@@ -1,23 +1,27 @@
 #!/usr/bin/env bash
+
+# Initilize DPCPP
+source /opt/intel/oneapi/compiler/latest/env/vars.sh
 set -eu
 
 OPEN3D_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 OPEN3D_BUILD="${OPEN3D_ROOT}/build"
 
-clean_up() {
+clean_build() {
     pushd ${OPEN3D_ROOT}
     rm -rf ${OPEN3D_BUILD} || true
     mkdir ${OPEN3D_BUILD} || true
-    rm -f build_release.log
-    rm -f build_debug.log
     popd
 }
 
-# Source ISPC
-source /opt/intel/oneapi/compiler/latest/env/vars.sh
+clean_logs() {
+    rm -f build_release.log
+    rm -f build_debug.log
+}
+clean_logs
 
 # Release build
-clean_up
+clean_build
 pushd ${OPEN3D_BUILD}
 cmake \
     -DBUILD_CUDA_MODULE=OFF \
@@ -39,14 +43,14 @@ cmake \
     -DBUILD_UNIT_TESTS=ON \
     -DCMAKE_INSTALL_PREFIX=~/open3d_install .. \
     &> >(tee -a "../build_release.log")
-make VERBOSE=1 Open3D -j$(nproc) &> >(tee -a "../build_release.log")
+make VERBOSE=1 Open3D -j$(nproc) \
+    &> >(tee -a "../build_release.log")
+make tests -j 32 && time ./bin/tests --gtest_filter="*Tensor*Sqrt*" \
+    &> >(tee -a "../build_release.log")
 popd
 
-# Clean up
-clean_up
-
 # Debug build
-clean_up
+clean_build
 pushd ${OPEN3D_BUILD}
 cmake \
     -DBUILD_CUDA_MODULE=OFF \
@@ -68,7 +72,10 @@ cmake \
     -DBUILD_UNIT_TESTS=ON \
     -DCMAKE_INSTALL_PREFIX=~/open3d_install .. \
     &> >(tee -a "../build_debug.log")
-make VERBOSE=1 Open3D -j$(nproc) &> >(tee -a "../build_debug.log")
+make VERBOSE=1 Open3D -j$(nproc) \
+    &> >(tee -a "../build_debug.log")
+make tests -j 32 && time ./bin/tests --gtest_filter="*Tensor*Sqrt*" \
+    &> >(tee -a "../build_debug.log")
 popd
 
 
