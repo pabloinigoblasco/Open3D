a71bb7c11d37d9073667fd3b09fd2bacfe8b35ab
Yixing Lao <yixing.lao@gmail.com>
2021-09-23 03:08:13 -0700

    comments

--- cmake/Open3DSetGlobalProperties.cmake
+++ cmake/Open3DSetGlobalProperties.cmake
@@ -110,7 +110,8 @@ function(open3d_set_global_properties target)
     endif()
 
     # Turn off fast math for IntelLLVM DPC++ compiler
-    # TODO: make this optional and tune unit tests floating point precisions.
+    # Fast math is turned off for clang by default even for -O3.
+    # We may make this optional and tune unit tests floating point precisions.
     target_compile_options(${target} PRIVATE
         $<$<AND:$<CXX_COMPILER_ID:IntelLLVM>,$<NOT:$<COMPILE_LANGUAGE:ISPC>>>:-fno-fast-math>)
 
